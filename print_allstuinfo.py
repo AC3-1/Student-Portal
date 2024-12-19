b27a5e6defee60d33327e3eafda806938a4b4e42
Andrei Ancheta <dreiancheta20@gmail.com>
2024-12-19 10:21:22 +0800

    Update print_allstuinfo.py

--- print_allstuinfo.py
+++ print_allstuinfo.py
@@ -1,18 +1,14 @@
 from tkinter import *
 
 def view_all_stu_info_gui(parent_frame, students):
-    # Clear the parent frame
     for widget in parent_frame.winfo_children():
         widget.destroy()
 
-    # Title
     Label(parent_frame, text="All Students Information", font=("Century Gothic", 16), bg="white").pack(pady=10)
 
     if not students:
-        # If no students are in the system
         Label(parent_frame, text="No students in the system.", font=("Century Gothic", 14), bg="white", fg="red").pack(pady=20)
     else:
-        # Display all students
         for student in students:
             student_info = (
                 f"Name: {student.FullName()}\n"
